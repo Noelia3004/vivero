a9d10f25e8b8e01694c9e54e24140afb2b70bee4
Noelia3004 <t22gonzaleznoe@gmail.com>
2024-11-20 21:49:46 -0300

    Add files via upload

---
+++ styles/script.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("carousel-slide");
+    if (n >= slides.length) { slideIndex = 0 }
+    if (n < 0) { slideIndex = slides.length - 1 }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
